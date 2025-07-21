1b7463897cc1cc071618286251e9867477bba60d
Jahswill-web-dev <onuegbujahswill@gmail.com>
2025-07-21 11:41:46 +0100

    changed tailwind config file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,18 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 import ProgressBar from "@badrap/bar-of-progress";
-import Router from 'next/router';
+import Router from "next/router";
 
 const progress = new ProgressBar({
-  size:4,
-  color:"#FE59FE",
-  className:"z-index-50",
-  delay:100
+  size: 4,
+  color: "#FE59FE",
+  className: "z-index-50",
+  delay: 100,
 });
 
-
-Router.events.on("routeChangeStart", progress.start)
-Router.events.on("routeChangeComplete", progress.finish)
-Router.events.on("routeChangeError", progress.finish)
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
